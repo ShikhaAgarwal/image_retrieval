69ffd0fa4926b72d3614a92b3a97f3cd4d1e0e7a
Shikha Agarwal <shikha@Shikhas-MacBook-Pro.local>
2018-12-01 22:48:02 -0500

    testing ssh key

--- image_folder.py
+++ image_folder.py
@@ -1,23 +1,7 @@
-# from torch.utils.data import Dataset
 from torchvision.datasets.folder import ImageFolder
 import glob
 
 class MyImageFolder(ImageFolder):
-    # def __init__(self, folder_path, transform ):
-    #     super(ImageFolder, self).__init__(root=folder_path, transform=transform)
-    #     self.files = sorted(glob.glob('%s/*.*' % folder_path))
-    #     #print (self.files)
-    #     self.transform = transform
-
-    # def __getitem__(self, index):
-    #     img_path = self.files[index % len(self.files)]
-    #     # Extract image
-    #     img = np.array(Image.open(img_path))
-
-    #     return img_path, imgx
-
-    # def __len__(self):
-    #     return len(self.files)
 
     def __getitem__(self, index):
         data = super(MyImageFolder, self).__getitem__(index)
