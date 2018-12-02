1d07474cbb530fc7944041fbbc75b214763065b0
Shikha Agarwal <shikha@Shikhas-MacBook-Pro.local>
2018-12-01 19:57:15 -0500

    storing features in a file

--- loader.py
+++ loader.py
@@ -1,5 +1,3 @@
-# from __future__ import print_function, division
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,31 +8,32 @@ import torchvision
 from torchvision import datasets, models, transforms
 from torchvision.models.vgg import model_urls
 import matplotlib.pyplot as plt
-import time
 import os
 import copy
+import h5py
 
-plt.ion()  
+# plt.ion()  
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
     print("Using CUDA")
 
-data_dir = '/Users/shikha/Documents/Fall2018/ComputerVision/Project/dataset'
-TRAIN = 'train/'
+data_dir = '/Users/shikha/Documents/Fall2018/ComputerVision/Project/image_retrieval/dataset/'
+TRAIN = 'train'
 VAL = 'val'
 TEST = 'test'
+batch_size = 3
+# ---- PLEASE CHANGE FILE NAME BELOW ----
+phase = TRAIN
+out_features_size = 4096
 
 print [os.path.join(data_dir, x) for x in [TRAIN, VAL, TEST]]
 # VGG-16 Takes 224x224 images as input, so we resize all of them
 data_transforms = {
     TRAIN: transforms.Compose([
-        # Data augmentation is a good practice for the train set
-        # Here, we randomly crop the image to 224x224 and
-        # randomly flip it horizontally. 
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
+        transforms.Scale((224, 224)),
         transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     VAL: transforms.Compose([
         transforms.Resize(256),
@@ -42,9 +41,9 @@ data_transforms = {
         transforms.ToTensor(),
     ]),
     TEST: transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Scale((224, 224)),
         transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 }
 
@@ -58,14 +57,14 @@ image_datasets = {
 
 dataloaders = {
     x: torch.utils.data.DataLoader(
-        image_datasets[x], batch_size=8,
+        image_datasets[x], batch_size=batch_size,
         shuffle=True, num_workers=4
     )
     for x in [TRAIN, VAL, TEST]
 }
 
 dataset_sizes = {x: len(image_datasets[x]) for x in [TRAIN, VAL, TEST]}
-print dataset_sizes
+print dataset_sizes[TRAIN]
 
 for x in [TRAIN, VAL, TEST]:
     print("Loaded {} images under {}".format(dataset_sizes[x], x))
@@ -77,15 +76,29 @@ print(image_datasets[TRAIN].classes)
 # Load the pretrained model from pytorch
 model_urls['vgg19_bn'] = model_urls['vgg19_bn'].replace('https://', 'http://')
 pre_model = torchvision.models.vgg19_bn(pretrained=True)
-print pre_model
-# model = nn.Sequential(*list(pre_model.children())[:-2])
-# print model
-model = pre_model.features
+# model takes all the layers except the last classification layer
+layers = list(pre_model.classifier.children())[0]
+pre_model.classifier = nn.Sequential(*[layers])
+# model.features = pre_model.features
 
-for param in model.parameters():    #----> 1
+for param in pre_model.parameters():    #----> 1
     param.requires_grad = False
 
-inputs, labels = next(iter(dataloaders[TRAIN]))
-inputs, labels = Variable(inputs), Variable(labels)
-outputs = model(inputs)
-print outputs
+# get the features as matrix for each image
+# ------- SHOP -------
+num_samples = dataset_sizes[phase]
+train_result = np.zeros((num_samples, out_features_size))
+i = 0
+for data in dataloaders[phase]:
+    inputs, labels = data
+    inputs, labels = Variable(inputs), Variable(labels)
+    outputs = pre_model(inputs)
+    print outputs.shape
+    start = i
+    end = i+batch_size
+    train_result[start:end, :] = outputs
+    i += batch_size
+
+file_name = data_dir+phase+'_feature.h5'
+with h5py.File(file_name, 'w') as hf:
+    hf.create_dataset("shop_feature",  data=train_result)
