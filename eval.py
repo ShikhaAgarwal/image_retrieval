bc5dd0cd51652bb2dd9250c5f7637baa399e9a42
Shikha Agarwal <ag.ashikha1@gmail.com>
2018-12-03 15:37:37 -0500

    adding eval code to calculate hit and miss for exact matched images

---
+++ eval.py
@@ -0,0 +1,22 @@
+
+data_dir = '/Users/shikha/Documents/Fall2018/ComputerVision/Project/image_retrieval/dataset/'
+output_file = data_dir + "output"
+
+test_images = []
+matched_image = []
+with open(output_file,'rb') as f_in:
+    for line in f_in:
+        splitLine = line.strip().split(',\t')
+        test_images.append(splitLine[0])
+        matched_image.append(splitLine[1])
+
+hit = 0
+miss = 0
+for test, match in zip(test_images, matched_image):
+	if test == match:
+		hit += 1
+	else:
+		miss += 1
+
+assert(hit+miss == len(test_images))
+print hit, miss
