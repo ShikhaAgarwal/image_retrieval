294995b0d6d060903ad91a01e2aeace3eabc8e97
snehabhattacharya <sneha.22.7@gmail.com>
2018-12-07 16:52:19 -0500

    save top k

--- kmeans.py
+++ kmeans.py
@@ -6,16 +6,18 @@ import numpy as np
 import bottleneck as bn 
 import csv
 
-data_dir = '/Users/shikha/Documents/Fall2018/ComputerVision/Project/image_retrieval/dataset/'
-# data_dir = '/mnt/nfs/scratch1/snehabhattac/vision_data/copied_data/DRESSES/Dress/'
+#data_dir = '/Users/shikha/Documents/Fall2018/ComputerVision/Project/image_retrieval/dataset/'
+data_dir = '/mnt/nfs/scratch1/snehabhattac/vision_data/'
 TRAIN = 'train'
 VAL = 'val'
 TEST = 'test'
-num_cluster = 2
-top_k = 10
+num_cluster = 1
+top_k = 20
 clusters_out_file_name = data_dir + "clusters/skirt_cluster_name"
 clusters_out_file_data = data_dir + "clusters/skirt_cluster_data"
 output_file = data_dir + "output.csv"
+
+data_types = [VAL]
 
 def read_data(file_name, dataset_name, image_dataset_name):
     with h5py.File(file_name, 'r') as hf:
@@ -24,15 +26,15 @@ def read_data(file_name, dataset_name, image_dataset_name):
     return data, img_name
 
 phase = TRAIN 
-dataset_name = "shop_feature"
-image_dataset_name = "shop_feature_image"
-file_name = data_dir + phase + '_feature.h5'
+dataset_name = data_types[0] + "_features"
+image_dataset_name = data_types[0] + "_image_names"
+file_name = data_dir + data_types[0] + '_feature.h5'
 train_data, train_img_name = read_data(file_name, dataset_name, image_dataset_name)
 
-phase = TEST
-dataset_name = "consumer_feature"
-image_dataset_name = "consumer_feature_image"
-file_name = data_dir + phase + '_feature.h5'
+#phase = TEST
+#dataset_name = "consumer_feature"
+#image_dataset_name = "consumer_feature_image"
+file_name = data_dir + data_types[0] + '_feature1.h5'
 test_data, test_img_name = read_data(file_name, dataset_name, image_dataset_name)
 
 # ----- KMeans Cluster -----
@@ -55,10 +57,10 @@ for i in range(len(cluster_labels)):
 
 sorted_label_img_name = sorted(label_2_img_name.items(),key = lambda x:x[0])
 sorted_label_img_data = sorted(label_2_img_data.items(),key = lambda x:x[0])
-with open(clusters_out_file_name,'w') as f_out:
-    for label, images in sorted_label_img_name:
-        for img in images:
-            f_out.write(str(label)+'\t'+img+'\n')
+#with open(clusters_out_file_name,'w') as f_out:
+ #   for label, images in sorted_label_img_name:
+  #      for img in images:
+   #         f_out.write(str(label)+'\t'+img+'\n')
 
 # with open(clusters_out_file_data,'w') as f_out:
 #     for label, images in sorted_label_img_data:
@@ -73,20 +75,23 @@ for i, data in enumerate(test_data):
     cluster_imgs_name = label_2_img_name[closest_centers[i]]
     distance_data = np.zeros(len(cluster_imgs_data))    
     for j, img in enumerate(cluster_imgs_data):
-        dist = distance.eucledian(data, img)
+        dist = distance.euclidean(data, img)
         distance_data[j] = dist
         # if dist < min_distance:
         #     min_distance = dist
         #     matched_image = cluster_imgs_name[j]
     top_k_indices = bn.argpartition(distance_data,kth=top_k)
+    for idx,k in enumerate(top_k_indices):
+        result[test_img_name[i]].append(cluster_imgs_name[k])
+        if idx > top_k:
+            break
+    #result[test_img_name[i]] = cluster_imgs_name[top_k_indices][:top_k]
 
-    result[test_img_name[i]] = cluster_imgs_name[top_k_indices][:top_k]
 
-print result
 with open(output_file, 'w') as f_out:
     writer = csv.writer(f_out)
     for consumer, shop in result.items():
-        writer.writerow([consumer,shop])
+        writer.writerow([consumer,",".join(shop)])
 
   
         #f_out.write(consumer+',\t'+shop+'\n')
