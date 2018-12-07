0ba62e1d96a0e68da7d73a725f347fc4794c7784
Shikha Agarwal <ag.ashikha1@gmail.com>
2018-12-07 15:41:39 -0500

    saving image name to dataset

--- inference.py
+++ inference.py
@@ -10,8 +10,6 @@ TRAIN = 'train'
 VAL = 'val'
 TEST = 'test'
 
-use_gpu = torch.cuda.is_available() 
-print "gpu = ", use_gpu
 data_dir = '/mnt/nfs/scratch1/snehabhattac/vision_data/'
 model_dir = '/mnt/nfs/scratch1/shikhaagarwa/saved_model/'
 #model_dir = '/mnt/nfs/scratch1/snehabhattac/vision_data/output_weights/'
@@ -19,18 +17,31 @@ path = os.path.join(model_dir, '_15.weights')
 batch_size = 16
 data_types = [VAL]
 out_features_size = 4096
-dataset_name = data_types[0]
+
+dataset_name = data_types[0] + '_features'
+image_dataset_name = data_types[0] + '_image_names'
 phase = data_types[0]
+use_gpu = torch.cuda.is_available() 
+print "gpu = ", use_gpu
+
+def get_path_to_filename(paths):
+    filenames = []
+    for p in paths:
+        f = p.split('/')[-1].split('.')[0]
+        filenames.append(f)
+    return filenames
 
 vgg_model = model.FeatureExtractor()
-#print vgg_model
 vgg_model.load_state_dict(torch.load(path))
 
 dataloaders, num_samples = data_loader(data_dir, data_types, batch_size,mode=data_types[0])
 result = np.zeros((num_samples, out_features_size))
+result_filename = []
 i = 0
 for data in dataloaders[phase]:
     anchor, _, _, target, path = data
+    filenames = get_path_to_filename(paths)
+    result_filename += filenames
     if use_gpu:
         anchor = Variable(anchor.cuda())
     else:
@@ -44,3 +55,4 @@ for data in dataloaders[phase]:
 file_name = data_dir + data_types[0] + '_feature.h5'
 with h5py.File(file_name, 'w') as hf:
     hf.create_dataset(dataset_name, data=result)
+    hf.create_dataset(image_dataset_name, data=result_filename)
