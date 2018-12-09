735d8974138e349514772c5df68ec596c0126214
snehabhattacharya <sneha.22.7@gmail.com>
2018-12-09 17:53:13 -0500

    crop image

--- crop_img_script.py
+++ crop_img_script.py
@@ -13,6 +13,7 @@ dest_dir = '/mnt/nfs/scratch1/snehabhattac/vision_data/processed_data/'
 anno_file = data_dir + 'meta/Anno/list_bbox_consumer2shop.txt'
 category = 'TROUSERS'
 
+partition_file = "dataset/list_eval_partition_val.txt"
 i=0
 shop_bbox = defaultdict(int)
 consumer_bbox = defaultdict(int)
@@ -26,8 +27,8 @@ with open(anno_file,'rb') as f_in:
         splitLine = line.strip().split()
         path = splitLine[0].split('/')
         bbox = splitLine[-4:]
-        if path[1] != category:
-            continue
+        #if path[1] != category:
+        #    continue
 
         img_id = path[3]
         path_dict[splitLine[0]] = bbox
@@ -38,46 +39,78 @@ with open(anno_file,'rb') as f_in:
             consumer_bbox[key] += 1
 
 # path is not a list here
-
+#print path_dict.keys()
+i = 0
 with open(partition_file, "rb") as p_file:
     check_set = set()
     for line in p_file:
+        i += 1
+        line = line.strip().split("\t")
         p_1, p_2, _ = line
-        if p_1 in patch_dict.keys():
-            if p_1 not in check_set:
-                x1 = path_dict[p_1][0]
-                y1 = path_dict[p_1][1]
-                x2 = path_dict[p_1][2]
-            	y2 = path_dict[p_1][3]
-                p_1_img = os.path.join(data_dir,p_1)
-                p_1_img = cv2.imread(p_1_img)
-	        crop_img_1 = p_1_img[y1:y2, x1:x2]
-                check_set.add(p_1)
-                dest_ = os.path.join(dest_dir,p_1)
-                if not os.path.exists(dest_):
-		    os.makedirs(dest_)
-                cv2.imwrite(dest_,crop_img_1)
-        else:
-            cv2.imwrite(dest_, p_1_img)
+        p_1_img = os.path.join(data_dir,p_1)
+        p_1_img = cv2.imread(p_1_img)
+        p_2_img = os.path.join(data_dir,p_2)
+        p_2_img = cv2.imread(p_2_img)
+  
+        if p_1 not in check_set:
+            if p_1 in path_dict.keys():
+                #print p_1
+                #if p_1 not in check_set:
+            	x1 = int(path_dict[p_1][0])
+            	y1 = int(path_dict[p_1][1])
+            	x2 = int(path_dict[p_1][2])
+            	y2 = int(path_dict[p_1][3])
+	    	crop_img_1 = p_1_img[y1:y2, x1:x2]
+            	check_set.add(p_1)
+                fname = p_1.split("/")[-1]
+                dir_ = p_1.split("/")[:-1]
+                #print dir_, fname
+            	dest_ = os.path.join(dest_dir,"/".join(dir_))
+                
+            	if not os.path.exists(dest_):
+	        	os.makedirs(dest_)
+            	cv2.imwrite(dest_+ "/"+fname,crop_img_1)
+            	#print "cropped", p_1
+            else:
+                fname = p_1.split("/")[-1]
+                dir_ = p_1.split("/")[:-1]
+            	dest_ = os.path.join(dest_dir,"/".join(dir_))
+            	if not os.path.exists(dest_):
+                    os.makedirs(dest_)
+            	cv2.imwrite(dest_,"/"+fname,crop_img_1)
+            	print "not found", p_1
 
-       
-        if p_2 in patch_dict.keys():
-            if p_2 not in check_set:
-                x1 = path_dict[p_2][0]
-                y1 = path_dict[p_2][1]
-                x2 = path_dict[p_2][2]
-                y2 = path_dict[p_2][3]
-                p_2_img = os.path.join(data_dir,p_2)
-                p_2_img = cv2.imread(p_1_img)
-                crop_img_2 = p_2_img[y1:y2, x1:x2]
-                check_set.add(p_2)
-                dest_ = os.path.join(dest_dir,p_2)
+        if p_2 not in check_set:
+            if p_2 in path_dict.keys():
+            	# _2 not in check_set:
+            	x1 = int(path_dict[p_2][0])
+            	y1 = int(path_dict[p_2][1])
+            	x2 = int(path_dict[p_2][2])
+            	y2 = int(path_dict[p_2][3])
+            	crop_img_2 = p_2_img[y1:y2, x1:x2]
+            	check_set.add(p_2)
+                fname = p_2.split("/")[-1]
+                dir_ = p_2.split("/")[:-1]
+
+                dest_ = os.path.join(dest_dir,"/".join(dir_))
+
+                if not os.path.exists(dest_):
+                        os.makedirs(dest_)
+                cv2.imwrite(dest_+ "/"+fname,crop_img_2)
+                #print "cropped", p_1
+
+            	
+            	#print "cropped", p_2
+            else:
+                fname = p_2.split("/")[-1]
+                dir_ = p_2.split("/")[:-1]
+                dest_ = os.path.join(dest_dir,"/".join(dir_))
                 if not os.path.exists(dest_):
                     os.makedirs(dest_)
-                cv2.imwrite(dest_,crop_img_2)
-        else:
-            cv2.imwrite(dest_, p_2_img)
-        print line
+                cv2.imwrite(dest_,"/"+fname,crop_img_2)
+                print "not found p_2", p_2
+
+       # print i
 
         
 
